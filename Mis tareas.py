23db644a3524c9a1e865271de7853f8dfc7c0733
LUCELY MAYA <ll.mayae@uea.edu.ec>
2024-02-25 14:18:21 -0500

    DEBER DE LA SEMANA 11

---
+++ Mis tareas.py
@@ -0,0 +1,15 @@
+#Empezamos creando nuestra variable dimensional
+b=[
+    ['zapatos,camisa,corbata'],
+    ['shots,zapatillas,pantalonestas'],
+    ['gorra,chaqueta,sombrero']
+    ]
+#imprimimos nuestra variable original
+
+print("Arreglo Bibimensional 3x3 original ",b)
+#haciendo uso del sorf ordenamos los valores de nuestra variable de forma ascendente
+
+b.sort()
+#inprimimos la variable  ordenadad
+print("Arreglo Bibimensional 3x3 ordenado ",b)
+#con esto finalizando mi codigo
